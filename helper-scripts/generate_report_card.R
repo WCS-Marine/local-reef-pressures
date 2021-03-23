a2ad03cf5b2d990e5b8b27a3daa1090316a876fb
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-03-23 11:47:23 -0400

    Change report card save location

--- helper-scripts/generate_report_card.R
+++ helper-scripts/generate_report_card.R
@@ -4,7 +4,7 @@
 #' @param open Whether to open the PDF once the report card is generated. Defaults to TRUE.
 #' @param quiet Whether to suppress messages about which report card is being produced and where it is saved. Defaults to FALSE.
 #'
-#' @return A set of pdf report cards saved in \code{analysis/report_cards/}
+#' @return A set of pdf report cards saved in \code{report-cards/}
 #' @export
 generate_report_cards <- function(bcu = "all", open = TRUE, quiet = FALSE) {
 
@@ -33,14 +33,14 @@ generate_report_cards <- function(bcu = "all", open = TRUE, quiet = FALSE) {
 #' @param quiet Whether to suppress messages about which report card is being produced and where it is saved. Defaults to FALSE
 #' @param standalone Whether the report card is being produced standalone (i.e., only one report card is being produced) - defaults to TRUE. This controls whether the libraries and data are loaded when the function is called.
 #'
-#' @return A pdf report card saved in \code{analysis/report_cards/}
+#' @return A pdf report card saved in \code{analysis/report-cards/}
 #' @export
 #'
 #' @examples
 #' generate_single_report_card("Eel - NGBR")
 generate_single_report_card <- function(bcu, open = TRUE, quiet = FALSE, standalone = TRUE) {
 
-  fs::dir_create(here::here("report_cards"))
+  fs::dir_create(here::here("report-cards"))
 
   if (length(bcu) > 1) {
     stop("This function can only generate a report card for a single BCU.\nPlease use `generate_report_cards` to generate for multiple BCUs.")
@@ -56,7 +56,7 @@ generate_single_report_card <- function(bcu, open = TRUE, quiet = FALSE, standal
   bcu_name <- stringr::str_replace_all(bcu_name, "/| ", "-")
   bcu_name <- stringr::str_replace_all(bcu_name, "---", "-")
   bcu_country <- stringr::str_replace_all(bcu_country, " ", "-")
-  out_file <- here::here("report_cards", glue::glue("{bcu_country}_{bcu_name}.pdf"))
+  out_file <- here::here("report-cards", glue::glue("{bcu_country}_{bcu_name}.pdf"))
 
   rmarkdown::render(
     input = here::here("scripts", "report-cards", "99-report_card_template.Rmd"),
